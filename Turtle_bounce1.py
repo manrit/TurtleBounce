b5f2175c74c2e69905a248807119cfd318b14baf
Manrit <75276753+manrit@users.noreply.github.com>
2022-05-20 17:23:41 -0700

    Update Turtle_bounce1.py

--- Turtle_bounce1.py
+++ Turtle_bounce1.py
@@ -1,6 +1,7 @@
-# BY MANRIT KAUR 
+# BY MANRIT KAUR
 # MAY 20 2022
-from tabnanny import check
+# Turtle Bounce Assignment
+
 import turtle
 import random
 
@@ -9,8 +10,19 @@ window = turtle.Screen()
 window.screensize(SIZE, SIZE)
 window.title("Turtle Bounce")
 
+
 Turtle = turtle.Turtle()
 Turtle.shape("turtle")
+
+#Drawing box
+Turtle.penup()
+Turtle.setposition(-250, -250)
+Turtle.pendown()
+#square
+for _ in range(4):
+  Turtle.forward(SIZE) # Forward turtle by s units
+  Turtle.left(90) # Turn turtle by 90 degree
+
 Turtle.penup()
 Turtle.setposition(0, 0)
 Turtle.forward(5)
@@ -21,24 +33,21 @@ def randomDirection():
   return direction
 
 def Check_edges(t):
-  
   while True:
     #randonDirection() method called 
     t.setheading(randomDirection())  
-    
     count = 0  #number of times Turtle moved forward
     x, y = t.position()  # Get current x,y position
 
-  
-      # Keep moving until it crosses the window size of 500
-     while abs(x) <= SIZE and abs(y) <= SIZE:
+    #if the x,y position is not == size then keep moving turtle
+    if abs(x) != SIZE and abs(y) != SIZE: 
+      # Keep moving until turtle crosses the window size of 500
+     while abs(x) <= SIZE /2 and abs(y) <= SIZE/2 :
         t.forward(5)
         count += 1
         x, y = t.position()
-  
-    # Move Turtle -5 steps back
+    # Move Turtle -5 steps back every loop
     for i in range(count):
         t.forward(-5)
 
 Check_edges(Turtle)
-window.exitonclick()
